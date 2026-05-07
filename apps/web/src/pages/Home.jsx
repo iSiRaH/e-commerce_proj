0c19edb3c734a400822e30aaa949fd0703adeaa4
iSiRaH <isirahansaja@gmail.com>
2026-05-08 00:14:10 +0530

    Make the project in monorepo and setup pnpm package manager and other necessary dependancies for development in frontend and backend

---
+++ apps/web/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500">Home</h1>
+      <p className="text-red-500">Welcome to the E commerce site!</p>
+    </>
+  );
+};
+
+export default Home;
